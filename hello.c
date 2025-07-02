53a5ea990fc35f031389add5e172bc53c32dba82
MohdRavishIdreesi <rawishidreesi241999@outlook.com>
2025-07-02 10:33:41 +0530

    from test edited c file

--- hello.c
+++ hello.c
@@ -2,5 +2,7 @@
 #include <conio.h>
 int main(){
     printf("Hello, World!\n");
+    // Uncomment the line below if you want to use getch()
+    // getch(); // Wait for a key press
     return 0;
 }
